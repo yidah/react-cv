68e5facb48525b0332ddb2e0413febd896d6fb7d
Yidah <yidahit@hotmail.com>
2021-02-08 17:48:04 +0000

    Added Project squares for Portfolio page

--- src/components/Porfolio/Project/Project.js
+++ src/components/Porfolio/Project/Project.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import classes from './Project.module.css';
 
 const project = (props)=>{
     return(
         <>
-        <div>
-          <img src={props.imgSrc} alt={props.imgAlt}/>
-        </div>
-        <div>
-          <h2>{props.project}</h2>
-          <a href={props.codeHref}>View Code</a>
-          <p>{props.desc}</p>
-        </div>
+            <div className={classes.CardContainer}>
+              <div className={classes.Card}>Burger Builder</div>
+              <div className={classes.Text}>
+                <span class="bold">ChowNow Ordering</span>
+                <span class="highlight">React JS / Python</span>
+              </div>
+              <div className={classes.Button}>
+                LEARN MORE
+              </div>
+            </div>
         </>
     )
 }
--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -1,22 +1,23 @@
-import React,{ Component } from "react";
+import React, { Component } from 'react';
 import Project from '../../components/Porfolio/Project/Project';
-import BurgerBuilder from '../../assets/images/react-burger-builder.png';
+// import BurgerBuilder from '../../assets/images/react-burger-builder.png';
 import classes from './Portfolio.module.css';
 
-class Portfolio extends Component{
-    render(){
-        return(
-            <div className={classes.main}>
-            <Project imgSrc={BurgerBuilder}
-                     imgAlt="Burger Builder"
-                     codeHref="https://github.com/yidah/burger-builder"
-                     project="Burger Builder"
-                     desc="My project is super chulo"
-                     />
-            </div>
-
-        )
-    }
+class Portfolio extends Component {
+  render() {
+    return (
+      <>
+        <div className={classes.Gallery} >
+          <Project/>
+          <Project/>
+          <Project/>
+          <Project/>
+          <Project/>
+          <Project/>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Portfolio;
