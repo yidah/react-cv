7ec34e02f550a2d13786f4b82bedffa7622b9d83
Yidah <yidahit@hotmail.com>
2021-02-09 10:37:01 +0000

    Added project cards with flip effect

--- src/components/Porfolio/Project/Project.js
+++ src/components/Porfolio/Project/Project.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import classes from './Project.module.css';
 
-const project = (props)=>{
-    return(
-        <>
-            <div className={classes.CardContainer}>
-              <div className={classes.Card}>Burger Builder</div>
-              <div className={classes.Text}>
-                <span class="bold">ChowNow Ordering</span>
-                <span class="highlight">React JS / Python</span>
-              </div>
-              <div className={classes.Button}>
-                LEARN MORE
-              </div>
-            </div>
-        </>
-    )
-}
+const project = (props) => {
+  return (
+    <>
+      <div className={classes.CardContainer}>
+        <div className={classes.Card}>
+          <div className={classes.Front}>Burger Builder</div>
+          <div className={classes.Back}>
+            <span className={classes.Description}>ChowNow Ordering</span>
+            <span className={classes.Tech}>React JS / Python</span>
+            <span className={classes.Button}>React JS / Python</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default project;
