3805d678ed9fcbe957f7b11d4f62da9c7dbfcd35
Yidah <yidahit@hotmail.com>
2021-02-11 15:54:47 +0000

    Added animated contact envelope to the Contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,22 +1,35 @@
-import React, { Component } from "react";
-import Media from '../../components/Contact/Media/Media';
+import React, { Component } from 'react';
+import classes from './Contact.module.css';
+// import Media from '../../components/Contact/Media/Media';
 
-class Contact extends Component{
-    render(){
-        return(
-            <div>
-                <div>Contact</div>
-                <form>
-                    <input type="Text" placeholder="Name"/>
-                    <input type="email" placeholder="e-mail"/>
-                    <input type="Text" placeholder="Message"/>
-                    <input type="submit" value="SUBMIT"/>
-                </form>
-                <Media/>
-            </div>
+class Contact extends Component {
+  render() {
+    return (
+      <>
+      <div className={classes.EnvelopeContainer}>
+      <div className={classes.Envelope} >
+          <div className={classes.Back}></div>
+          <form className={classes.Letter}>
+            <p>
+              <label>Name:</label>
+              <input type="text" name="name" />
+            </p>
+            <p>
+              <label>Enter Email:</label>
+              <input type="email" name="email" />
+            </p>
+            <p>
+              <label>Your Message:</label>
+              <textarea></textarea>
+            </p>
+            <input className={classes.Button} type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
 
-        )
-    }
+      </>
+    );
+  }
 }
 
 export default Contact;
