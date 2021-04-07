8d8730a50f11fd18c8982bc7bac5603029987908
Yidah <yidahit@hotmail.com>
2021-04-07 18:09:36 +0100

    Added media queries in Home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,14 +5,19 @@ class Home extends Component {
   render() {
     return (
       <>
-        <div className={classes.CircleBig}></div>
-        <div className={classes.CircleMedium}></div>
-        <div className={classes.CircleSmall}></div>
+        <div className={classes.Home}>
+          <div className={classes.CircleBig}></div>
+          <div className={classes.CircleBig2}></div>
+          <div className={classes.CircleMedium}></div>
+          <div className={classes.CircleMedium2}></div>
+          <div className={classes.CircleSmall}></div>
+          <div className={classes.CircleSmall2}></div>
 
-        <div className={classes.IntroContainer}>
-          <div className={classes.IntroOne}>Hi! I'm Yidah Aguirre</div>
-          <div className={classes.IntroTwo}>a front-end developer</div>
-          <div className={classes.IntroThree}>have a look at my work </div>
+          <div className={classes.IntroContainer}>
+            <div className={classes.IntroOne}>Hi! I'm Yidah Aguirre</div>
+            <div className={classes.IntroTwo}>a front-end developer</div>
+            <div className={classes.IntroThree}>have a look at my work </div>
+          </div>
         </div>
       </>
     );
